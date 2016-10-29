347b2f00bb0e7c35f70466572468c80727b2befa
MW Lee <ionizn@gmail.com>
2016-10-30 08:38:48 +0900

    워프도어 추가

---
+++ Assets/CheateKey.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheateKey : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("h"))
+		{
+			Application.LoadLevel("Ending2");
+		}
+	}
+}
