5a12d7a69fb2231ab42ab9f608828607067cbd03
MW Lee <ionizn@gmail.com>
2016-10-30 06:50:01 +0900

    1버전 업로드

---
+++ Assets/Stage/Scripts/EnemySpawner.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour 
+{
+
+	[SerializeField]
+     public float range = 5.0f;
+ 
+     private Transform t;
+     public Transform player;
+ 
+     private void Awake()
+     {
+         t = this.transform;
+		 InvokeRepeating( "CheckDistance", 1f, 1f);
+		 gameObject.SetActive(false);
+	 }
+ 
+     public void CheckDistance()
+     {
+         if(player)
+		 {
+			float  dist = Vector3.Distance(transform.position, player.position);
+			if(dist <= range)
+			{
+				Debug.Log("Dist : "+ dist);
+				 gameObject.SetActive(true);
+				CancelInvoke("CheckDistance");
+			}
+		 }
+	 }
+
+	private void OnDrawGizmos(){
+#if UNITY_EDITOR		
+		UnityEditor.Handles.color = Color.yellow;
+		UnityEditor.Handles.DrawWireDisc(this.transform.position ,Vector3.back, range);
+#endif
+	}
+
+
+
+}
---
+++ Assets/Stage/Scripts/StageStart.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.anyKeyDown)
+		{
+			OnGameStartBtnClicked();
+		}
+	}
+	public void OnGameStartBtnClicked()
+	{
+		Application.LoadLevel("Tutorial");
+	}
+}
